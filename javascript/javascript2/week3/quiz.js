cfb5b0ed2fc18422217b46911c6a8c74c9a9d30b
nigerabed <nigerabed@gmail.com>
2025-03-26 00:07:36 +0100

    javascript week-3 added shuffle array function

---
+++ javascript/javascript2/week3/quiz.js
@@ -0,0 +1,104 @@
+
+ import questions from "./quizData.json" with { type: "json" };
+
+
+const urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
+const sequence = urlParams.get('sequence');
+console.log(sequence);
+
+if(sequence === "random"){
+    function shuffle(questionArr){
+        // Fisher- yates shuffle algorithm 
+
+        for(let i = questionArr.length-1 ; i > 0; i--){
+            let random = Math.floor(Math.random() * (i + 1));
+            [questionArr[i], questionArr[random]] = [questionArr[random], questionArr[i]];
+        }
+    }
+    shuffle(questions);
+}
+
+
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex  = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+};
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+const selectedBtn = e.target;
+const isCorrect = selectedBtn.dataset.correct === "true";
+if(isCorrect){
+    selectedBtn.classList.add("correct");
+    score++
+}else{
+    selectedBtn.classList.add("incorrect"); 
+}
+Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+        button.classList.add("correct");
+    }
+    button.disabled = true;
+});
+nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = ` you scroed ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+currentQuestionIndex++;
+if(currentQuestionIndex < questions.length){
+    showQuestion();
+}else{
+    showScore();
+}
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
