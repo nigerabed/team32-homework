92cdc873c62ea24753c6f4d8cf1e95a5fed0bbcd
nigerabed <nigerabed@gmail.com>
2025-03-25 22:09:16 +0100

    javascript2 week-2 ; i added question list and add form for add new question. and show correct ans button

--- javascript/javascript2/week2/script.js
+++ javascript/javascript2/week2/script.js
@@ -1,129 +1,89 @@
-const questions = [
-    {
-      question: "Which is the largest animal in the world?",
-      answers: [
-        { text: "Shark", correct: false },
-        { text: "Blue whale", correct: true },
-        { text: "Elephant", correct: false },
-        { text: "Giraffe", correct: false },
-      ],
-    },
-    {
-      question: "Which is the smallest continent in the world?",
-      answers: [
-        { text: "Kalahari", correct: false },
-        { text: "Gobi", correct: false },
-        { text: "Sahara", correct: false },
-        { text: "Antarctica", correct: true },
-      ],
-    },
-    {
-      question: "Which is the largest desert in the world?",
-      answers: [
-        { text: "Asia", correct: false },
-        { text: "Australia", correct: true },
-        { text: "Arctic", correct: false },
-        { text: "Africa", correct: false },
-      ],
-    },
-    {
-      question: "What is the capital of Japan?",
-      answers: [
-        { text: "Beijing", correct: false },
-        { text: "Seoul", correct: false },
-        { text: "Tokyo", correct: true },
-        { text: "Bangkok", correct: false },
-      ],
-    },
-    {
-      question: "Which planet is known as the Red Planet?",
-      answers: [
-        { text: "Earth", correct: false },
-        { text: "Mars", correct: true },
-        { text: "Jupiter", correct: false },
-        { text: "Venus", correct: false },
-      ],
-    },
-  ];
+import questions from "./quizData.json" with { type: "json" };
+console.log("questions", questions);
 
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
 
-let currentQuestionIndex  = 0;
-let score = 0;
-
-function startQuiz(){
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
+const addQuestionBtn = document.getElementById("addQuestionBtn");
+addQuestionBtn.addEventListener("click", handleOpenForm);
+function handleOpenForm(){
+    const Form = document.getElementById("addQuestion");  
+    Form.classList.toggle("formHide"); 
 }
 
-function showQuestion(){
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        if(answer.correct){
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-    })
-};
 
-function resetState(){
-    nextButton.style.display = "none";
-    while(answerButtons.firstChild){
-answerButtons.removeChild(answerButtons.firstChild);
-    }
+function handleShowCorrectAnswer(event){
+    console.log("looooog");
+    const btnSpan = event.target;
+    const sibling = btnSpan.nextElementSibling; // Get the next sibling
+    sibling.classList.toggle("hide");
+ 
+}
+function randerQuestions(){
+    const ol = document.getElementById("ol");
+    ol.innerHTML = "";
+    questions.forEach(question =>{
+        const li = document.createElement("li");
+        li.className = "allQuestions-style";
+        let allQuestionsSpan = document.createElement("span");
+        allQuestionsSpan.innerText = question.question;
+        li.appendChild(allQuestionsSpan);
+        let showAnswerBtn = document.createElement("button");
+        showAnswerBtn.classList.add("showAnswerBtn");
+        showAnswerBtn.innerText = "show answer"
+        li.appendChild(showAnswerBtn);
+        let showAnsBtnSpan = document.createElement("span");
+        const correctAnswer = question.answers.find(answer => answer.correct === true);
+        showAnsBtnSpan.classList.toggle("hide");
+        showAnsBtnSpan.innerText = correctAnswer.text;
+
+        showAnsBtnSpan.style.background = "green";
+        showAnsBtnSpan.style.color = "#fff";
+        li.appendChild(showAnsBtnSpan);
+        showAnswerBtn.addEventListener("click", handleShowCorrectAnswer);
+
+
+        const optionsUl = document.createElement("ul");
+        question.answers.forEach(answer =>{
+               const optionsLi = document.createElement("li");
+               optionsLi.innerHTML = answer.text;
+               optionsUl.appendChild(optionsLi);
+           });
+        li.appendChild(optionsUl)
+        ol.appendChild(li);
+    });
+    
 }
 
-function selectAnswer(e){
-const selectedBtn = e.target;
-const isCorrect = selectedBtn.dataset.correct === "true";
-if(isCorrect){
-    selectedBtn.classList.add("correct");
-    score++
-}else{
-    selectedBtn.classList.add("incorrect"); 
-}
-Array.from(answerButtons.children).forEach(button => {
-    if(button.dataset.correct === "true"){
-        button.classList.add("correct");
-    }
-    button.disabled = true;
-});
-nextButton.style.display = "block";
-}
+randerQuestions();
 
-function showScore(){
-    resetState();
-    questionElement.innerHTML = ` you scroed ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play again";
-    nextButton.style.display = "block";
-}
 
-function handleNextButton(){
-currentQuestionIndex++;
-if(currentQuestionIndex < questions.length){
-    showQuestion();
-}else{
-    showScore();
-}
-}
+const addQuestionForm = document.getElementById("addQuestion");
+addQuestionForm.addEventListener("submit", handleSubmit);
 
-nextButton.addEventListener("click", ()=>{
-    if(currentQuestionIndex < questions.length){
-        handleNextButton();
-    }else{
-        startQuiz();
-    }
-})
 
-startQuiz();
+function handleSubmit(e){
+
+    e.preventDefault();
+    const q = document.getElementsByName("question");
+    const inputTextA = document.getElementsByName("textA");
+    const inputTextB = document.getElementsByName("textB");
+    const inputTextC = document.getElementsByName("textC");
+    const inputTextD = document.getElementsByName("textD");
+
+    const correctAns = document.getElementsByName("correctAns");
+ 
+    
+   const newQuestion =  {
+        "question":  q[0].value,
+        "answers": [
+          { "text": inputTextA[0].value, "correct": correctAns[0].checked },
+          { "text": inputTextB[0].value, "correct": correctAns[1].checked },
+          { "text": inputTextC[0].value, "correct": correctAns[2].checked },
+          { "text": inputTextD[0].value, "correct": correctAns[3].checked }
+        ]
+      }
+
+      questions.push(newQuestion);
+
+      randerQuestions();
+    console.log(questions)
+}
